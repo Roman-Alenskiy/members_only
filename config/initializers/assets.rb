9d403c7aba7a961808ba7e399aa60541347f13f9
Roman Alenskiy <kuperman88@gmail.com>
2018-05-08 21:45:40 +0300

    Sign up and Log in refactored

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w( static_pages.css )
+Rails.application.config.assets.precompile += %w( custom.css )
