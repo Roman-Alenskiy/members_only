40b0b6b9b241c858d333c098326fe27a3fb37819
Roman Alenskiy <kuperman88@gmail.com>
2018-05-11 16:26:30 +0300

    Finished User edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     validates :email, presence: true, length: {maximum:255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 
     has_secure_password
-    validates :password, presence: true, length: {minimum:6, maximum: 60}
+    validates :password, presence: true, length: {minimum:6, maximum: 60}, allow_nil: true
 
     # Remember user in database for auto log in
     def remember
